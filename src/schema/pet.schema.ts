280798222d09dffeff5119f997f3d1c6d51879ca
luke-h1 <luke.howsam@yahoo.com>
2024-07-29 21:45:22 +0100

    feat(api): basic crud

--- src/schema/pet.schema.ts
+++ src/schema/pet.schema.ts
@@ -1,37 +1,39 @@
 import { z } from 'zod';
 
 const payload = {
-  body: z.object({
-    name: z.string({
-      required_error: 'Name is required',
-      description: 'Name of the pet',
-    }),
-    description: z.string({
-      required_error: 'Description is required',
-      description: 'Description of the pet',
-    }),
-    age: z.number({
-      required_error: 'Age is required',
-      description: 'Age of the pet',
-    }),
-    breed: z.string({
-      required_error: 'Breed is required',
-      description: 'Breed of the pet',
-    }),
-    available: z.boolean({
-      required_error: 'Available is required',
-      description: 'Availability of the pet',
-    }),
-    species: z.string({
-      required_error: 'Species is required',
-      description: 'Species of the pet',
-    }),
-    tags: z.array(
-      z.string({
-        description: 'Tags of the pet',
+  body: z
+    .object({
+      name: z.string({
+        required_error: 'Name is required',
+        description: 'Name of the pet',
       }),
-    ),
-  }),
+      description: z.string({
+        required_error: 'Description is required',
+        description: 'Description of the pet',
+      }),
+      age: z.number({
+        required_error: 'Age is required',
+        description: 'Age of the pet',
+      }),
+      breed: z.string({
+        required_error: 'Breed is required',
+        description: 'Breed of the pet',
+      }),
+      available: z.boolean({
+        required_error: 'Available is required',
+        description: 'Availability of the pet',
+      }),
+      species: z.string({
+        required_error: 'Species is required',
+        description: 'Species of the pet',
+      }),
+      tags: z.array(
+        z.string({
+          description: 'Tags of the pet',
+        }),
+      ),
+    })
+    .strict(),
 };
 
 const params = {
