97e718cf80554d4f89b05c8dcda5688a058b3c12
luke-h1 <luke.howsam@yahoo.com>
2024-08-11 16:58:37 +0100

    feat(ci): add e2e support in CI

---
+++ scripts/setup-e2edb.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+trap 'echo -e "\033[31m✖ An error occurred. Exiting...\033[0m"; exit 1;' ERR
+
+DATABASE_URL='postgresql://pets:pets@localhost:5432/pets?schema=public' pnpm db:migrate-dev && DATABASE_URL='postgresql://pets:pets@localhost:5432/pets?schema=public' pnpm prisma db push
+echo -e "\033[32m✔ Test Database migrated\033[0m"
