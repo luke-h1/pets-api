16f401499b26e88b07d5fabf6efaffa6bab9cd08
luke-h1 <luke.howsam@yahoo.com>
2024-08-12 11:33:20 +0100

    fix(ci): fix build

--- scripts/setup-testdb.sh
+++ scripts/setup-testdb.sh
@@ -4,5 +4,5 @@ set -e
 
 trap 'echo -e "\033[31m✖ An error occurred. Exiting...\033[0m"; exit 1;' ERR
 
-DATABASE_URL='postgresql://pets:pets@localhost:5555/pets?schema=public' pnpm db:generate && DATABASE_URL='postgresql://pets:pets@localhost:5555/pets?schema=public' pnpm prisma db push --force-reset --force && DATABASE_URL='postgresql://pets:pets@localhost:5555/pets?schema=public' pnpm db:generate && DATABASE_URL='postgresql://pets:pets@localhost:5555/pets?schema=public' pnpm db:migrate-dev && DATABASE_URL='postgresql://pets:pets@localhost:5555/pets?schema=public' pnpm prisma db push
+DATABASE_URL='postgresql://pets:pets@localhost:5555/pets?schema=public' pnpm db:migrate-dev && DATABASE_URL='postgresql://pets:pets@localhost:5555/pets?schema=public' pnpm prisma db push
 echo -e "\033[32m✔ Test Database migrated\033[0m"
