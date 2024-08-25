42770d4cd84150aab75a03dfdf67ee7c84c41a4d
luke-h1 <luke.howsam@yahoo.com>
2024-08-25 23:30:33 +0100

    feat(api): add git sha to version

--- src/schema/version.schema.ts
+++ src/schema/version.schema.ts
@@ -3,6 +3,7 @@ import z from '@api/utils/validation';
 export const versionSchema = z
   .object({
     deployedAt: z.string(),
-    deployedby: z.string(),
+    deployedBy: z.string(),
+    gitSha: z.string(),
   })
   .strict();
